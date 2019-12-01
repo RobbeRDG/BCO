c41639e81a961ac65dd1767c46d6524c02834af7
RobbeRDG <robbedegroeve@gmail.com>
2019-12-01 16:40:43 +0100

    login fix 4

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var indexRouter = require('./routes/index');
-var userRouter = require('./routes/users');
-var compression = require('compression');
-var helmet = require('helmet');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/users');
+const compression = require('compression');
+const helmet = require('helmet');
+const MongoStore = require('connect-mongo')(session);
 
 
 
-var app = express();
+
+const app = express();
 
 app.use(helmet());
 
@@ -20,7 +22,17 @@ process.env.NODE_ENV = 'production';
 app.use(cookieParser());
 
 
-
+//Set up mongoose connection
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+const dev_db_url = 'mongodb+srv://Dairo:XBi8uM%26Q4L%23fpV-@cluster0-jxapn.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const  mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 //Session setup
@@ -35,6 +47,7 @@ app.use(session({
       secure: true,
       expires: 600000
   },
+  store: new MongoStore({ mongooseConnection: db })
 }));
 
 // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
@@ -50,17 +63,6 @@ if (app.get('env') === 'production') {
   app.set('trust proxy', 1) // trust first proxy
 }
 
-
-//Set up mongoose connection
-const mongoose = require('mongoose');
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-const dev_db_url = 'mongodb+srv://Dairo:XBi8uM%26Q4L%23fpV-@cluster0-jxapn.gcp.mongodb.net/test?retryWrites=true&w=majority';
-const  mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 
